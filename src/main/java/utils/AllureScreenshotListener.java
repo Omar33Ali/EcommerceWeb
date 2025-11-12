5f15540a3364b48698505212fa08aa8d1884c067
Omar33Ali <omar.ak0130@gmail.com>
2025-11-13 01:39:04 +0200

    WIP: saving changes before revert

---
+++ src/main/java/utils/AllureScreenshotListener.java
@@ -0,0 +1,29 @@
+package utils;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class AllureScreenshotListener implements ITestListener {
+    private WebDriver driver;
+
+    // Inject driver (you can set it via constructor or a static holder)
+    public AllureScreenshotListener(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        if (driver != null) {
+            captureScreenshot(driver, "Failure Screenshot - " + result.getName());
+        }
+    }
+
+    @Attachment(value = "{name}", type = "image/png")
+    public byte[] captureScreenshot(WebDriver driver, String name) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
