201b42c0d9aabe5b8921c3b1462ee06461b438b8
Omar33Ali <omar.ak0130@gmail.com>
2025-11-14 18:05:34 +0200

    fixing allure report on pipline

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,9 +3,9 @@ package base;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import utils.ConfigReader;
 import utils.DataReader;
 
 import java.time.Duration;
@@ -19,13 +19,38 @@ public class TestBase {
     @BeforeClass
     public void setUp() {
 
-        // Set up WebDriver and load configuration
-        driver = new ChromeDriver();
+        ChromeOptions options = buildChromeOptions();
+        driver = new ChromeDriver(options);
         // Initialize ConfigReader and load test data
         testData = DataReader.readFromResource("test_data.json");
         // Maximize the window and set implicit wait
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitDuration));
+    }
+
+    private ChromeOptions buildChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        if (shouldRunHeadless()) {
+            options.addArguments("--headless=new");
+        }
+        return options;
+    }
+
+    private boolean shouldRunHeadless() {
+        String headlessProp = System.getProperty("headless");
+        if (headlessProp != null) {
+            return Boolean.parseBoolean(headlessProp);
+        }
+        String ciEnv = System.getenv("CI");
+        if (ciEnv != null) {
+            return true;
+        }
+        String osName = System.getProperty("os.name", "").toLowerCase();
+        return osName.contains("linux") || osName.contains("unix");
     }
 
     @AfterMethod
